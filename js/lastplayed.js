c152a7ae2dae14b328c5e8d1270da8e2974d5313
Nikita Belov <nikita2010belov@gmail.com>
2026-03-30 08:28:39 +0500

    now last.played async; cat.fs.tlpteam.ru:5011 -> www.ballix.net

--- js/lastplayed.js
+++ js/lastplayed.js
@@ -2,20 +2,38 @@
 const lastPlayedBlock = document.getElementById('lastPlayed');
 
 // https://stackoverflow.com/questions/247483/http-get-request-in-javascript
-let xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", "https://cat.fs.tlpteam.ru:5011/?user=tolyagosuslugi", false);
-xmlHttp.send(null);
+// let xmlHttp = new XMLHttpRequest();
+// xmlHttp.open("GET", "https://cat.fs.tlpteam.ru:5011/?user=tolyagosuslugi", false);
+// xmlHttp.send(null);
 
-if (xmlHttp.responseText != 0) {
-    const lastParse = JSON.parse(xmlHttp.responseText);
+// if (xmlHttp.responseText != 0) {
+//     const lastParse = JSON.parse(xmlHttp.responseText);
     
+//     const br1 = document.createElement("br");
+//     const br2 = document.createElement("br");
+
+//     const lastPlayed = document.createElement("div");
+//     lastPlayed.setAttribute("class", "content__text");
+//     lastPlayed.innerHTML = (lastParse["nowplaying"] ? "now listening to" : "last played")
+//                             + ": <i><a href=\"" + lastParse["url"] + "\" target=\"_blank\">" + lastParse["artist"] + " - " + lastParse["name"] + "</a></i>";
+
+//     lastPlayedBlock.append(lastPlayed, br1, br2);
+// }
+
+async function fetchLastFM() {
+    const data = await fetch("https://www.ballix.net/whatsplaying/?user=tolyagosuslugi")
+
+    const res = await data.json();
+
     const br1 = document.createElement("br");
     const br2 = document.createElement("br");
 
     const lastPlayed = document.createElement("div");
     lastPlayed.setAttribute("class", "content__text");
-    lastPlayed.innerHTML = (lastParse["nowplaying"] ? "now listening to" : "last played")
-                            + ": <i><a href=\"" + lastParse["url"] + "\" target=\"_blank\">" + lastParse["artist"] + " - " + lastParse["name"] + "</a></i>";
+    lastPlayed.innerHTML = (res["nowplaying"] ? "now listening to" : "last played")
+                            + ": <i><a href=\"" + res["url"] + "\" target=\"_blank\">" + res["artist"] + " - " + res["name"] + "</a></i>";
 
     lastPlayedBlock.append(lastPlayed, br1, br2);
 }
+
+window.addEventListener("load", fetchLastFM)
